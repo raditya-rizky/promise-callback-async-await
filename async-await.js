d37f2f062c804d16aea9dc6ab0b1eaa89646244e
raditya-rizky <gusrizkydharmaputra123@gmail.com>
2022-06-06 10:52:10 +0800

    update file async-await

--- async-await.js
+++ async-await.js
@@ -11,9 +11,9 @@ async function main() {
   const data = await fs.readFile("tugas/backup.txt", "utf8");
   console.log(data);
 
-  await fs.copySync("tugas", "tugas-async-await");
-  await fs.removeSync("tugas");
-  const data2 = fs.readFileSync("tugas-async-await/backup.txt", "utf8");
+  await fs.copy("tugas", "tugas-async-await");
+  await fs.remove("tugas");
+  const data2 = fs.readFile("tugas-async-await/backup.txt", "utf8");
   console.log(data2);
 }
 
